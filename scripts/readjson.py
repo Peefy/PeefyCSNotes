0c7eef1363b373af050db22736d12040eac3ee44
Peefy <xpf6677@163.com>
2019-12-05 15:35:22 +0800

    Update

--- scripts/readjson.py
+++ scripts/readjson.py
@@ -5,3 +5,10 @@ if __name__ == "__main__":
     with open('./scripts/ast.json') as fd:
         obj = json.load(fd)
         print(obj)
+    with open('./scripts/lefttree.json') as fd:
+        obj = json.load(fd)
+        print(obj)
+    with open('./scripts/righttree.json') as fd:
+        obj = json.load(fd)
+        print(obj)
+
